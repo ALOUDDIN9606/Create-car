618a53a020b83261022f0c23211704a3fa9f726b
lazizjon5818 <lazizjonmurodullayev7@gmail.com>
2024-12-19 02:25:08 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='flex items-center justify-center h-7 container '><h2 className='text-4xl'>lazizjon</h2></div>
+    </>
+  )
+}
+
+export default App
